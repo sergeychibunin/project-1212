fa4805352f25c7550a87d6dad6d41a7b97d070c9
Sergey Chibunin <chibunin1988@gmail.com>
2020-10-31 19:37:01 +0300

    Update models

--- mediaportal/core/models.py
+++ mediaportal/core/models.py
@@ -30,15 +30,27 @@ id
 title
 """
 
-class Page(models.Model):
-    title = models.CharField(max_length=100)
-
-
 class ContentVideo(models.Model):
     title = models.CharField(max_length=100)
-    pages = models.ManyToManyField(Page)
+    video_url = models.URLField()
+    subtitles_url = models.URLField()
+    counter = models.PositiveIntegerField()
 
 
 class ContentAudio(models.Model):
     title = models.CharField(max_length=100)
-    pages = models.ManyToManyField(Page)
+    rate = models.IntegerField()
+    counter = models.PositiveIntegerField()
+
+
+class ContentText(models.Model):
+    title = models.CharField(max_length=100)
+    body = models.TextField()
+    counter = models.PositiveIntegerField()
+
+
+class Page(models.Model):
+    title = models.CharField(max_length=100)
+    videos = models.ManyToManyField(ContentVideo)
+    audios = models.ManyToManyField(ContentAudio)
+    texts = models.ManyToManyField(ContentText)
