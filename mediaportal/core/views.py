23ae2a20873e4b2ff7ac3af63b90a62476469cd0
Sergey Chibunin <chibunin1988@gmail.com>
2020-11-01 17:39:13 +0300

    Add the page list endpoint

--- mediaportal/core/views.py
+++ mediaportal/core/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework import generics
+from core.models import Page
+from core.serializers import PageSerializer
 
-# Create your views here.
+
+class PageListView(generics.ListAPIView):
+    queryset = Page.objects.all()
+    serializer_class = PageSerializer
