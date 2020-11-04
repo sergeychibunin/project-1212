180126696430b2a3471ec8a5f3b58491f74c5385
Sergey Chibunin <chibunin1988@gmail.com>
2020-11-04 15:23:57 +0300

    Add tests

--- mediaportal/core/tests.py
+++ mediaportal/core/tests.py
@@ -1,3 +1,33 @@
-from django.test import TestCase
+from rest_framework import status
+from rest_framework.test import APITestCase
+from unittest.mock import patch
+from django.urls import reverse
+from core.models import Page, ContentText
 
-# Create your tests here.
+
+class PageListTestCase(APITestCase):
+
+    def test_user_can_get_page_list(self):
+        Page.objects.create(title='p1')
+        Page.objects.create(title='p2')
+        response = self.client.get(reverse('page-list'))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        r = response.json()
+        self.assertEqual('count' in r, True)
+        self.assertEqual('previous' in r, True)
+        self.assertEqual('next' in r, True)
+        self.assertEqual('results' in r, True)
+        self.assertEqual(r['results'][0]['url'].startswith('http'), True)
+
+    @patch('core.tasks.increase_counter.delay')
+    def test_user_can_get_page_detail(self, cel_task):
+        t1 = ContentText.objects.create(title='t1', body='b1', counter=0)
+        t2 = ContentText.objects.create(title='t2', body='b2', counter=0)
+        p1 = Page.objects.create(title='p1')
+        p1.texts.add(t2)
+        p1.texts.add(t1)
+        response = self.client.get(reverse('page-detail', args=[p1.id]))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        r = response.json()
+        self.assertEqual(r == {'id': 1, 'title': 'p1', 'texts': [{'title': 't2', 'body': 'b2'}, {'title': 't1', 'body': 'b1'}], 'audios': [], 'videos': []}, True)
+        self.assertEqual(cel_task.called, True)
